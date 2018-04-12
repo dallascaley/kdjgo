b9eb688cd5f711d1cb5872e2d758d5c7794b4ef5
Dallas Caley <dallas.caley@corp.updatepromise.com>
2018-04-12 08:08:41 -0700

    Adding ngx bootstrap

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,6 +2,7 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { HttpClientModule } from '@angular/common/http';
 import { AppRoutingModule } from './app-routing.module';
+import { AlertModule } from 'ngx-bootstrap';  //This is an example from ngx bootstrap doc, probably not actually needed (yet)
 
 //Components
 import { AppComponent } from './app.component';
@@ -22,6 +23,7 @@ import { SingerComponent } from './singer/singer.component';
 		BrowserModule,
 		HttpClientModule,
         AppRoutingModule,
+        AlertModule.forRoot(), //ditto bootstrap example
 	],
 		providers: [
 		SongService,
